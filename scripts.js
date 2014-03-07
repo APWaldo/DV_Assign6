e781f61e120b4032577e961028ad6591bac267bc
APWaldo <apw2131@columbia.edu>
2014-03-06 22:35:17 -0500

    Adding console logs at beginning and end of scripts just to double check things.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,32 @@
 /**
  * @author Annie P Waldman
  */
+//Console log to make sure that I know that this works.
+console.log ("Hi there. About to make a pretty cool visualization about remittances.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Putting an end page console log just for double checking things.
+console.log ("This is the end page console log. Just for double checking things.");
