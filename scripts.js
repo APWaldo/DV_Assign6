401919b4d16689771bb5405d0f0dd42c2e582e28
APWaldo <apw2131@columbia.edu>
2014-03-07 11:06:04 -0500

    Adding googleloaded to link to my fusion table data source.

--- scripts.js
+++ scripts.js
@@ -2,26 +2,20 @@
  * @author Annie P Waldman
  */
 //Console log to make sure that I know that this works.
-console.log ("Hi there. About to make a pretty cool visualization about remittances.");
+console.log ("Hi there. About to make a pretty cool visualization about cell phones.");
 
 
 
+//Instead of adding data from a static json file, I'm going to load it from a google fusion table. I really really hope
+//that this works.
+function googleLoaded(){
+	//console logging it for good measure.
+	console.log ("Google has loaded")
+	
+	//Time to load data with get function. This will tell my page to go and get this data set from my link.
+	$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+1sevSyWbei7h7H0B2qM2HyIZ0arPiODIw7Qa1_yaj&key=AIzaSyC_YvjF2i4ejxQorXr0ge-tvfwv4HQrQoA", dataLoaded, "json");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 //Adding my new function pageLoaded and console logging to make sure that the pageLoaded function activates on
@@ -34,7 +28,7 @@ function pageLoaded () {
 	//Load the google visualization library with the callback googleLoaded. This will tell the browser to load the function
 	//googleLoaded. This is using the google visualization script to work. But now I have to make sure that I have
 	//my function googleLoaded working.
-	google.load("visualization", "1", {packages:["corechart"], callback: "googleLoaded"});
+	google.load("visualization", "1", {packages:["geochart"], callback: "googleLoaded"});
 	//now I have to define my function googleLoaded next.....	
 	
 }
